f62f01201a0dcc8676dc487b186ed8d1ba2b3f33
AntoineCKuhn <antoine.kuhn@hashicorp.com>
2022-08-21 00:21:39 +0200

    First commit

---
+++ main.tf
@@ -0,0 +1,29 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "hashistack_resource_group" {
+  name     = "dach-se-hashistack"
+  location = "West Europe"
+}
+
+resource "azurerm_virtual_network" "hashistack_network" {
+  name                = "hashistack_network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.hashistack_resource_group.location
+  resource_group_name = azurerm_resource_group.hashistack_resource_group.name
+}
+
+resource "azurerm_subnet" "hashistack_subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.hashistack_resource_group.name
+  virtual_network_name = azurerm_virtual_network.hashistack_network.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+data "azurerm_platform_image" "vm_image" {
+  location  = "West Europe"
+  publisher = "Canonical"
+  offer     = "UbuntuServer"
+  sku       = "20.04-LTS"
+}
---
+++ server.tf
@@ -0,0 +1,94 @@
+locals {
+  nomad_apt   = length(split("+", var.nomad_version)) == 2 ? "nomad-enterprise" : "nomad"
+  consul_apt  = length(split("+", var.consul_version)) == 2 ? "consul-enterprise" : "consul"
+  vault_apt   = length(split("+", var.vault_version)) == 2 ? "vault-enterprise" : "vault"
+  #kms_key_id  = var.vault_enabled ? aws_kms_key.vault.0.key_id : "NULL"
+  #cert        = var.vault_tls_enabled ? tls_locally_signed_cert.vault.0.cert_pem : "NULL"
+  #key         = var.vault_tls_enabled ? tls_private_key.vault.0.private_key_pem : "NULL"
+  #ca_cert     = var.vault_tls_enabled ? tls_private_key.ca.0.public_key_pem : "NULL"
+  protocol    = var.vault_tls_enabled ? "https" : "http"
+  tls_disable = var.vault_tls_enabled ? "false" : "true"
+  
+  fqdn_tls    = [for i in range(var.server_count) : format("%s-%02d.%s", var.server_name, i +1, var.dns_domain)]
+  user_data_vars = {
+        server_count        = var.server_count
+        data_dir            = var.data_dir
+        datacenter          = var.datacenter
+        region              = var.region
+        nomad_join          = var.tag_value
+        node_name           = format("${var.server_name}-%02d",1)
+        nomad_enabled       = var.nomad_enabled
+        nomad_version       = var.nomad_version
+        nomad_apt           = local.nomad_apt
+        nomad_lic           = var.nomad_lic
+        nomad_bootstrap     = var.nomad_bootstrap
+        consul_enabled      = var.consul_enabled
+        consul_version      = var.consul_version
+        consul_apt          = local.consul_apt
+        consul_lic          = var.consul_lic
+        vault_enabled       = var.vault_enabled
+        vault_version       = var.vault_version
+        vault_apt           = local.vault_apt
+        vault_lic           = var.vault_lic
+        kms_key_id          = local.kms_key_id
+        azure_region          = var.azure_region
+        protocol            = local.protocol
+        tls_disable         = local.tls_disable
+        cert                = local.cert 
+        key                 = local.key
+        ca_cert             = local.ca_cert
+        dns_domain          = var.dns_domain
+    }  
+}
+
+
+resource "azurerm_network_interface" "hashistack_net_interface" {
+  count = 2
+  name                = "example_nic_${count.index}"
+  location            = azurerm_resource_group.hashistack_resource_group.location
+  resource_group_name = azurerm_resource_group.hashistack_resource_group.name
+
+  ip_configuration {
+    name                          = "internal__${count.index}"
+    subnet_id                     = azurerm_subnet.hashistack_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.hashistack_public_ip.*.id[count.index]
+  }
+}
+
+resource "azurerm_public_ip" "hashistack_public_ip" {
+  count = 2
+  name                = "hashistack_public_ip_${count.index}"
+  resource_group_name = azurerm_resource_group.hashistack_resource_group.name
+  location            = azurerm_resource_group.hashistack_resource_group.location
+  allocation_method   = "Static"
+
+  tags = {
+    environment = "test"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "hashistack_server_vm" {
+  count = 2
+  name                = "example-machine-${count.index}"
+  resource_group_name = azurerm_resource_group.hashistack_resource_group.name
+  location            = azurerm_resource_group.hashistack_resource_group.location
+  size                = "Standard_B1s"
+  admin_username      = "hashistack"
+  source_image_id = data.azurerm_platform_image.vm_image.id
+  network_interface_ids = [
+    azurerm_network_interface.hashistack_net_interface.*.id[count.index],
+  ]
+
+  admin_ssh_key {
+    username   = "hashistack" #upload an sshkey to the azure sub
+    public_key = file("~/Documents/SSH-Keys/Antoine-SSH-Key.pub")
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+  
+  user_data = templatefile("${path.root}/templates/server.sh", local.user_data_vars)
+}
